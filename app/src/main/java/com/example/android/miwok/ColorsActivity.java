bebab600767b2c0002bd2bab0c6ce556b199f03d
AhmadMahrous <ahmedsayed70422@gmail.com>
2017-12-10 23:28:42 +0200

    Miwok

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -8,6 +8,7 @@ public class ColorsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_colors);
 
 
